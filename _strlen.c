fd76ee03777be5857883b375a048ebc4ab994cce
Lawal Fadlurahman <70358533+kingfad@users.noreply.github.com>
2022-05-13 19:02:38 +0100

    Update _strlen.c

--- _strlen.c
+++ _strlen.c
@@ -1,1 +1,17 @@
-binbash
+#include "shell.h"
+
+/**
+ * _strlen - function that gets the length of a string
+ * @string: string to be evaluated.
+ * Return: length of the string
+ */
+
+int _strlen(char *string)
+{
+	int i = 0;
+
+	while (string[i])
+		i++;
+
+	return (i);
+}
